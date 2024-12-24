d020791c21d5f0a2871b2b417bf4422d0a99ad8c
Sagar Patel <sagarpatel04sr@gmail.com>
2024-12-24 22:30:45 +0530

    change in files

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { connectionToMongoDb } = require("./connnection")
 
 const urlRout = require("./routes/url")
 const userRoute=require("./routes/user")
+const staticRouter=require("./routes/staticRouter")
 
 const URL = require('./models/url')
 
@@ -13,12 +14,13 @@ const port = 8001
 connectionToMongoDb('mongodb://localhost:27017/short_url')
     .then(() => console.log("MongoDB Connected"))
 
-    app.set("view engine","ejs")
-    app.set("view",path.resolve*"./views")
+    app.set("view engine", "ejs");
+    app.set("views",path.resolve("./views"))
      
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 app.use("/url", urlRout)
 app.use("/user", userRoute)
-
+app.use("/",staticRouter)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
